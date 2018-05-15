34c11404fcfbb33e3bdd3c58d674c45d8227bbfd
edel <edel.altares@gmail.com>
2018-05-14 18:27:11 -0600

    Added simple 'ping-pong' functionality

---
+++ bot.js
@@ -0,0 +1,66 @@
+// Date: 2018-05-14
+// Author: Edel Altares
+// Description: Discord bot for the CSUS UCalgary server
+
+var auth = require('./auth.json');
+var logger = require('winston');
+var Discord = require('discord.io');
+
+// logger settings
+logger.level = 'debug';
+logger.remove(logger.transports.Console);
+logger.add(logger.transports.Console, {
+  colorize: true
+});
+
+logger.info(auth.token);
+
+// initialize the bot
+var bot = new Discord.Client({
+  token: auth.token,
+  autorun: true
+});
+
+logger.info("Bot created");
+
+bot.on('ready', function (event) {
+  logger.info('Bot connected to server');
+  logger.info('Logged in as: ' + bot.username + ' - (' + bot.id + ')');
+});
+
+
+
+bot.on('message', function(user, userID, channelID, message, event) {
+    if (message === "ping") {
+        bot.sendMessage({
+            to: channelID,
+            message: "pong"
+        });
+    }
+});
+
+bot.on('disconnect', function(error, code) {
+  logger.error(error);
+  logger.error(code);
+});
+
+bot.on('message', function (user, userID, channelID, message, event) {
+  // look for messages starting with '!'
+  if (message.substring(0,1) == '!'){
+    var args = message.substring(1).split(' ');
+    var cmd = args[0];
+
+    args = args.splice(1);
+
+    switch(cmd) {
+      // !ping
+      case 'ping':
+        bot.sendMessage({
+          to: channelID,
+          message: 'Pong!'
+        });
+        break;
+        
+    }
+  }
+});
