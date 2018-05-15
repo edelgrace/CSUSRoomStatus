5374ac5ccbe120c9f662bde501a3354f41150039
edel <edel.altares@gmail.com>
2018-05-15 11:57:12 -0600

    Added recognizing .csus callout

--- bot.js
+++ bot.js
@@ -21,32 +21,54 @@ var bot = new Discord.Client({
 
 logger.info("Bot created");
 
+// log connection
 bot.on('ready', function (event) {
   logger.info('Bot connected to server');
   logger.info('Bot logged in as: ' + bot.username + ' - (' + bot.id + ')');
 });
 
+// log any errors
 bot.on('disconnect', function(error, code) {
   logger.error(error);
   logger.error(code);
 });
 
+// check the messages
 bot.on('message', function (user, userID, channelID, message, event) {
   msg = message.trim();
   msg = msg.toLowerCase();
 
+  // check if a bot command
   if(msg.startsWith(".csus")) {
-
-    switch(cmd) {
-      // !status
+    botMsg = "";
+    
+    switch(msg) {
+      // CSUS ROOM STATUS
       case '.csus room':
+        botMsg = "The CSUS room is ";
+
+        // send a response
         bot.sendMessage({
           to: channelID,
-          message: 'The CSUS room is '
+          message: botMsg
         });
+
         break;
-        
+
+      // COMMAND NOT RECOGNIZED
+      default:
+        botMsg = "Are you trying to talk to @CSUSBot? Your command was not recognized";
+
+        bot.sendMessage({
+          to: channelID,
+          message: botMsg
+        });
+
     }
+
+    // log the interaction
+    logger.info("[" + user + "]: " + msg);
+    logger.info("[CSUSBot]: " + botMsg);
   }
   
 });
