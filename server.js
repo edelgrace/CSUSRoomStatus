c7a4877190c3ade3340113c925eab993ef0b9fab
Edel <me@edelgrace.me>
2018-09-23 16:18:26 -0600

    Changed logging to console output

--- server.js
+++ server.js
@@ -17,27 +17,27 @@ net.createServer(function(socket) {
 
   socket.write("Welcome " + socket.name + "\n");
   broadcast(socket.name + " joined\n", socket);
-  logger.log(socket.name + " joined\n");
+  console.log(socket.name + " joined\n");
 
   socket.on('data', function(data) {
     broadcast(socket.name + "> " + data, socket);
-    logger.log(socket.name + "> " + data)
+    console.log(socket.name + "> " + data)
   });
 
   socket.on('end', function() {
     clients.splice(clients.indexOf(socket), 1);
     broadcast(socket.name + "left\n");
-    logger.log(socket.name + "left\n");
+    console.log(socket.name + "left\n");
   });
 
   function broadcast(message, sender) {
     clients.forEach(function (client) {
       if (client === sender) return;
       client.writer(message);
-      logger.log(message);
+      console.log(message);
     });
   }
 }).listen(5000);
 
-logger.log("Welcome!");
+console.log("Welcome!");
 console.log("Welcome everyone!"); 
