cbed207d6ea39cc3dc110de49587fb54747b966c
edel <edel.altares@gmail.com>
2018-09-23 20:47:04 -0600

    More help message formatting

--- bot.js
+++ bot.js
@@ -13,8 +13,8 @@ var clients = [];
 
 const channel = config.channel;
 
-var HELP_MSG = "```.csus room \t get the room status \n "
-HELP_MSG    += ".csus arduino \t get the arduino status \n\n```" 
+var HELP_MSG = "```.csus room \t\t get the room status\n "
+HELP_MSG    += ".csus arduino \t get the arduino status\n\n```" 
 HELP_MSG    += "Questions? Email us at **csus@ucalgary.ca** \n"
 HELP_MSG    += "Contribute! **<https://github.com/edelgrace/CSUSRoomStatus>**";
 
